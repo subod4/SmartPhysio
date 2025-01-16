3a529ce46b063fa3722e787b2cf444bef1db3de9
Subodh Badal <subodhbadal111@gmail.com>
2025-01-16 23:29:25 +0545

    login/register backend final

--- node/src/Routes/AuthRouter.js
+++ node/src/Routes/AuthRouter.js
@@ -1,10 +1,13 @@
+const { signup, login } = require('../Controllers/AuthController');
+const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
+
+
 const router = require('express').Router();
 
 router.post('/login',(req,res)=>{
     res.send('login sucess'); 
 })
 
-router.post('/signup',(req,res)=>{
-    res.send('signup sucess'); 
-})
+router.post('/signup',signupValidation, signup);
+router.post('/login',loginValidation, login);
 module.exports = router;
